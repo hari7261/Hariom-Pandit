b4d856e9341c1bf2e1deb2093dfe583ed7a7ee17
HARIOM KUMAR PANDIT <hariompandit5556@gmail.com>
2025-01-26 21:25:27 +0530

    Remove GitHub activity and stats components from the project

--- src/pages/Guestbook.jsx
+++ src/pages/Guestbook.jsx
@@ -1,8 +1,6 @@
 import GiscusComponent from "../components/GiscusComponent";
 import SecondaryPara from "../ui/SecondaryPara";
 import SectionHeading from "../ui/SectionHeading";
-import GithubActivity from "../components/GithubActivity";
-import GithubStats from "../components/GithubStats";
 
 export default function Guestbook() {
   return (
@@ -10,14 +8,11 @@ export default function Guestbook() {
       id="guestBook"
       className="relative z-10 mx-auto space-y-5 px-5 py-10 text-white md:px-10 lg:px-20"
     >
-      <SectionHeading>My Github Contributions and Stats</SectionHeading>
+      <SectionHeading> Give Feedback By Github Discussion</SectionHeading>
+      <GiscusComponent />
       <SecondaryPara classname="text-xl">
         Leave a message in Guscus {`(>_<)`}
       </SecondaryPara>
-      <GithubActivity />
-      <GithubStats />
-      <GiscusComponent />
-
-    </div>
+    </div >
   );
 }
